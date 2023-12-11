f939c37c25e3567eeed12da6d77877301e94e300
luos <563349135@qq.com>
2023-12-11 13:09:23 +0800

    目录变更

--- hello-world/src/main/java/com/luos/controller/TestController.java
+++
@@ -1,2 +0,0 @@
-package com.luos.controller;public class TestController {
-}
---
+++ src/main/java/com/luos/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.luos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @GetMapping(value = "/hello")
+    private String hello() {
+        return "Hello World";
+    }
+
+}
